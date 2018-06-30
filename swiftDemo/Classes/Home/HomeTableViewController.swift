51dcacc5a7f1293e23cf8cae35230f73d80fe23f
唐欢 <tanghuan@tanghuandeMac-mini.local>
2018-06-30 15:25:30 +0800

    自定义转场动画

--- swiftDemo/Classes/Home/HomeTableViewController.swift
+++ swiftDemo/Classes/Home/HomeTableViewController.swift
@@ -53,6 +53,7 @@ extension HomeTableViewController{
         popover.modalPresentationStyle = .custom
         // 转场动画
         popover.transitioningDelegate = self
+    
         present(popover, animated: true) {
             
         }
@@ -62,9 +63,52 @@ extension HomeTableViewController{
 // MARK-代理
 extension HomeTableViewController: UIViewControllerTransitioningDelegate {
     
+    // 目的：设置弹出视图frame
     func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
         
        return THUIPresentationController(presentedViewController: presented, presenting: presenting)
         
+    }
+    
+  // 目的：设置弹出视图动画，调用这个函数，就是自定义动画，系统不会帮助完成动画，需要自己完成
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {// 返回的是一个遵循协议的对象，即返回self，self遵循协议
+        return self// self要遵循代理
+    }
+}
+
+
+// MARK-设置代理
+extension HomeTableViewController: UIViewControllerAnimatedTransitioning {
+    
+   // 动画时间
+    public func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval{
+        return 0.5
+    }
+    
+    // 添加动画
+    public func animateTransition(using transitionContext: UIViewControllerContextTransitioning){
+
+        
+        // 1、获取动画view
+        //UITransitionContextFromViewKey
+        //UITransitionContextToViewKey
+        //let presentView = transitionContext.viewForKey(UITransitionContextToViewKey)  这个是swift2的写法
+        guard let presentView = transitionContext.view(forKey: UITransitionContextViewKey.to) else{
+            
+            return
+        }
+        
+        
+        // 2.添加视图
+        transitionContext.containerView.addSubview(presentView)
+        presentView.layer.anchorPoint = CGPoint(x: 0.5, y: 0)
+        
+        // 3.执行动画
+        presentView.layer.transform = CATransform3DMakeScale(1, 0, 1)
+        UIView.animate(withDuration: transitionDuration(using: transitionContext), animations: {
+            presentView.layer.transform = CATransform3DIdentity
+        }) { (_) in
+            transitionContext.completeTransition(true)
+        }
     }
 }
