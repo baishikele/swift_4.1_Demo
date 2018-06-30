5d392bf6dc834d912065eb8805ba4c16add90286
唐欢 <tanghuan@tanghuandeMac-mini.local>
2018-06-30 10:03:55 +0800

    transitioningDelegate转场动画，自定义UIPresentationController类

--- swiftDemo/Classes/Home/HomeTableViewController.swift
+++ swiftDemo/Classes/Home/HomeTableViewController.swift
@@ -22,7 +22,7 @@ class HomeTableViewController: BaseTableViewController {
 
 }
 
-
+// MARK-UI处理
 extension HomeTableViewController{
     
      func setupHomeUIs() {
@@ -41,16 +41,30 @@ extension HomeTableViewController{
     }
 }
 
+// MARK-事件处理
 extension HomeTableViewController{
     
    @objc func titleClick(_ btn: TitleBtn) -> () {
         
         btn.isSelected = !btn.isSelected
+    
         let popover = UIPopoverViewController()
         // 这个枚举可以使下面的视图控制器始终都在
         popover.modalPresentationStyle = .custom
+        // 转场动画
+        popover.transitioningDelegate = self
         present(popover, animated: true) {
             
         }
+    }
+}
+
+// MARK-代理
+extension HomeTableViewController: UIViewControllerTransitioningDelegate {
+    
+    func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
+        
+       return THUIPresentationController(presentedViewController: presented, presenting: presenting)
+        
     }
 }
---
+++ swiftDemo/Classes/Home/UIPopover/THUIPresentationController.swift
@@ -0,0 +1,39 @@
+//
+//  THUIPresentationController.swift
+//  swiftDemo
+//
+//  Created by 唐欢 on 2018/6/30.
+//  Copyright © 2018年 唐欢. All rights reserved.
+//
+
+import UIKit
+
+class THUIPresentationController: UIPresentationController {
+
+    override func containerViewWillLayoutSubviews() {
+        
+        // 修改presentedView frame
+        presentedViewController.view.frame = CGRect(x: (presentedView?.frame.size.width)!/2-100, y: 55, width: 200, height: 300)
+        
+        // 设置蒙版
+        setupCover()
+    }
+}
+
+extension THUIPresentationController {
+    
+    func setupCover() -> () {
+        let cover = UIButton(frame: presentingViewController.view.bounds)
+        cover.addTarget(self, action: #selector(coverClick(_:)), for: .touchUpInside)
+        containerView?.insertSubview(cover, belowSubview: presentedViewController.view)
+        
+        cover.backgroundColor = UIColor(white: 0, alpha: 0.15)
+
+    }
+    
+    @objc func coverClick(_ btn:UIButton){
+        presentedViewController.dismiss(animated: true) {
+            
+        }
+    }
+}
