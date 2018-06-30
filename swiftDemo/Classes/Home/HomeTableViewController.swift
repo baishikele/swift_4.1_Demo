eb9590bc4f3b9584293642523735ba4bd8c29111
唐欢 <tanghuan@tanghuandeMac-mini.local>
2018-06-30 08:06:49 +0800

    UIPopoverko控制器

--- swiftDemo/Classes/Home/HomeTableViewController.swift
+++ swiftDemo/Classes/Home/HomeTableViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class HomeTableViewController: BaseTableViewController {
 
-    var titleBtn: TitleBtn = TitleBtn()
+   lazy var titleBtn: TitleBtn = TitleBtn()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,10 +32,24 @@ extension HomeTableViewController{
         }else{
             navigationItem.leftBarButtonItem = UIBarButtonItem(imageName: "navigationbar_friendattention")
             navigationItem.rightBarButtonItem = UIBarButtonItem(imageName: "navigationbar_pop")
-
+            titleBtn.addTarget(self, action: #selector(titleClick(_:)), for: .touchUpInside)
             titleBtn.setTitle("唐欢", for: .normal)
             navigationItem.titleView = titleBtn
             
+            
+        }
+    }
+}
+
+extension HomeTableViewController{
+    
+   @objc func titleClick(_ btn: TitleBtn) -> () {
+        
+        btn.isSelected = !btn.isSelected
+        let popover = UIPopoverViewController()
+        // 这个枚举可以使下面的视图控制器始终都在
+        popover.modalPresentationStyle = .custom
+        present(popover, animated: true) {
             
         }
     }
